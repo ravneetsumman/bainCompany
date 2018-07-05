57cc7204c4938f49652a746e283296091d9703fc
Ravneet Kaur <ravneetsumman@gmail.com>
2018-07-05 03:10:48 -0700

    whitenose settings added

--- healthproviders/healthproviders/wsgi.py
+++ healthproviders/healthproviders/wsgi.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 #from healthproviders import api as application
+from whitenoise.django import DjangoWhiteNoise
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthproviders.settings")
 #application = api
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
