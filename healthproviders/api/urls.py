86fc44637d903f40abf7b592e56e37625a1f00cf
Ravneet Kaur <ravneetsumman@gmail.com>
2018-07-08 19:33:32 -0700

    remove suffix patterns for production

--- healthproviders/api/urls.py
+++ healthproviders/api/urls.py
@@ -1,11 +1,7 @@
 from django.urls import include, path
-from rest_framework.urlpatterns import format_suffix_patterns
 from .views import Providers
 from api import views
 
 urlpatterns = {
-    #url(r'^providers/$', Providers.as_view(), name="list"),
     path('providers/', Providers.as_view(), name="list")
 }
-
-urlpatterns = format_suffix_patterns(urlpatterns)
