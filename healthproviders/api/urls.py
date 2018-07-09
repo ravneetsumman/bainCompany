3f4c3e1b5e169da72d9b5e6b0149769bad8f9334
Ravneet Kaur <ravneetsumman@gmail.com>
2018-07-08 19:21:07 -0700

    modified url dispatcher

--- healthproviders/api/urls.py
+++ healthproviders/api/urls.py
@@ -1,10 +1,11 @@
-from django.urls import url, include
+from django.urls import include, path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import Providers
 from api import views
 
 urlpatterns = {
-    url(r'^providers/$', Providers.as_view(), name="list"),
+    #url(r'^providers/$', Providers.as_view(), name="list"),
+    path('providers/', Providers.as_view(), name="list")
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
