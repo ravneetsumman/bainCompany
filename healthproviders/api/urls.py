bd6695ca5f0d052331d5c00e9346e00297c7f820
Ravneet Kaur <ravneetsumman@gmail.com>
2018-07-09 01:35:27 -0700

    added re path to url disptacher

--- healthproviders/api/urls.py
+++ healthproviders/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from .views import Providers
 from api import views
 
 urlpatterns = [
-    path('providers/', Providers.as_view(), name="list")
+    #path('providers/', Providers.as_view(), name="list")
+    re_path(r'^providers/$', Providers.as_view(), name="list"),
 ]
