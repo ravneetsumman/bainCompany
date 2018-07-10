6733d505305f9e43cfcda8108901eeabecc83415
Ravneet Kaur <ravneetsumman@gmail.com>
2018-07-09 18:34:50 -0700

    test regex url pattern

--- healthproviders/api/urls.py
+++ healthproviders/api/urls.py
@@ -4,5 +4,6 @@ from api import views
 
 urlpatterns = [
     #path('providers/', Providers.as_view(), name="list")
-    re_path(r'^providers/$', Providers.as_view(), name="list"),
+    #re_path(r'^providers/$', Providers.as_view(), name="list"),
+    url(r'^providers/$', Providers.as_view(), name="list"),
 ]
--- healthproviders/healthproviders/urls.py
+++ healthproviders/healthproviders/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^', include('api.urls')),
+    #re_path(r'^', include('api.urls')),
+    url(r'^', include(api.urls)),
 ]
