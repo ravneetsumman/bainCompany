463cd3d911249a606f29baae411dfcf819fd5325
Ravneet Kaur <ravneetsumman@gmail.com>
2018-07-09 18:56:34 -0700

    added format suffix

--- healthproviders/api/urls.py
+++ healthproviders/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import include, path, re_path
 from .views import Providers
 from api import views
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     #path('providers/', Providers.as_view(), name="list")
     #re_path(r'^providers/$', Providers.as_view(), name="list"),
     url(r'^providers/$', Providers.as_view(), name="list"),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
