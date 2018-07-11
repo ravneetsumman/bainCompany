50d30d43f82cd51eb5ba12e2e4efdf59535d4a21
Ravneet Kaur <ravneetsumman@gmail.com>
2018-07-10 17:43:08 -0700

    django rest framework update

--- healthproviders/healthproviders/urls.py
+++ healthproviders/healthproviders/urls.py
@@ -21,7 +21,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    URLResolver(r'^$', include('api.urls')),
+    path('', include('api.urls'))
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
